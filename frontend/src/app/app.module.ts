d4e4934150cfc3148822bcf5821822f0e019e09d
unknown <extensiontlhareseng@gmail.com>
2023-02-28 13:37:59 +0200

    Navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
